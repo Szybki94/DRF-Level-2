b6423c824f2ea20436894df3781f4d98ec897842
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2024-02-17 15:26:18 +0100

    feat: quotes model created

--- backend/apps/quotes/models.py
+++ backend/apps/quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Quote(models.Model):
+    author = models.CharField(max_length=256)
+    body = models.TextField()
+    context = models.TextField()
+    source = models.TextField(max_length=256)
+    created_at = models.DateField(auto_now_add=True)
+
