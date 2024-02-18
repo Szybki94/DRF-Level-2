32428f334248657ea932e014cf8a25698aa31136
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2024-02-18 17:52:13 +0100

    feat: all CRUD views for quotes are DONE, pagination DONE

--- backend/apps/quotes/urls.py
+++ backend/apps/quotes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.quotes.views import QuoteListView
+from apps.quotes.views import (QuoteListView,
+                               QuoteReadUpdateDestroyView)
 
 urlpatterns = [
     path('quotes/', QuoteListView.as_view(), name="quote-list"),
+    path('quotes/<int:pk>/', QuoteReadUpdateDestroyView.as_view(), name="quote-detail-view"),
 ]
